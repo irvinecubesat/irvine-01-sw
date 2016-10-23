1de1b0917c396682ce1c4ab0d2ca0567a0afbbb8
Maurice Ling <maurice@linglink.com>
2016-10-22 22:25:40 -0700

    Add Google test/mock, Download Project modules, and eigen linear algebra library

---
+++ cmake/Modules/gmock.cmake
@@ -0,0 +1,15 @@
+#
+# add_gmock_test(<target> <sources>...)
+#
+#  Adds a Google Mock based test executable, <target>, built from <sources> and
+#  adds the test so that CTest will run it. Both the executable and the test
+#  will be named <target>.
+#
+function(add_gmock_test target)
+    add_executable(${target} ${ARGN})
+    target_link_libraries(${target} gmock_main)
+
+    add_test(${target} ${target})
+
+endfunction()
+
