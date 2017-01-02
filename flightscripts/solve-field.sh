108d308963d6f64c04f2e4a8d1882b7cc3b5a831
Maurice Ling <maurice@linglink.com>
2017-01-02 07:44:48 -0800

    Add flight scripts directory, solve-field.sh

---
+++ flightscripts/solve-field.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# script to run solve-field with default arguments for flight
+#
+# Configuration may be adjusted in astrometry.cfg
+#
+solve-field --no-fits2fits --no-verify --no-plots $*
+      
