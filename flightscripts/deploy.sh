d9c07bfe1343919f1f985ff671c72f818fb60ef0
Mark64 <markleehill@gmail.com>
2017-02-17 12:05:42 -0800

    Fixed bugs and improved logic for the deployment script.  Now ready for Team Power DSA script integration

--- flightscripts/deploy.sh
+++ flightscripts/deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 DEPLOY_DIR=/data/deployment
-# I had issues with that directory not existing,
-#  but I didn't want to create it here since I 
-#  use it for the loop below, so for now these are
-#  hard-coded paths.  They'll probably be fixed
-#  later
 DEPLOY_LOG=$DEPLOY_DIR/deploy.log
 ANTENNA_FLAG=$DEPLOY_DIR/antenna_deployed
 SATCOMM_FLAG=$DEPLOY_DIR/satcomm_enabled
@@ -35,6 +30,7 @@ ALL_DEPLOY_COMPLETE_FLAG=$DEPLOY_DIR/deploy_complete
 #  implementation
 log() {
     echo "`date`: $1" >> $DEPLOY_LOG
+    echo "`date`: $1" 
 }
 
 # get the elapsed time since initial deployment
@@ -44,7 +40,7 @@ getTime() {
 
 # increment the time elapsed since initial deployment
 incrementTime() {
-    echo "`getTime`" > $ELAPSED_TIME
+    echo "$((`getTime` + 1))" > $ELAPSED_TIME
 }            
 
 #log "starting deployment"
@@ -60,29 +56,43 @@ if [ ! -d $DEPLOY_DIR ]; then
     echo "0" > $ELAPSED_TIME
     echo "0" > $DSA_DEPLOYED_FLAG
     echo "0" > $ALL_DEPLOY_COMPLETE_FLAG
+    echo "-1" > $ANTENNA_DEPLOY_TIME
     log "finished setting initial flags"
 
     CURDIR=$PWD
     cd /sdcard
-    uvccapture -M -j -x2048 -y1536 -oinitialPicture
+    #uvccapture -M -j -x2048 -y1536 -oinitialPicture
     cd $CURDIR
 fi
 
 while [ `cat $ALL_DEPLOY_COMPLETE_FLAG` -ne 1 ]; do 
+
     # Increment the time every second
     sleep 1
     incrementTime
+
     # If it hasn't been one orbit since the antenna
     #  was deployed, disable satcomm to avoid issues
-    #  with the antenna shorting the board
-    if ( [ `cat $ANTENNA_FLAG` -ne 1 ] || [ ( getTime - `cat $ANTENNA_DEPLOY_TIME` ) -le 2*90*60 ] ) ; then
-        log "disabling satcomm"
-        #rm /critical_state/satcom*
+    #  with the antenna shorting the board at the
+    #  recommenation of Cal Poly
+    if [ `cat $ANTENNA_FLAG` -ne 1 ] || [ $(( `getTime` - \
+     `cat $ANTENNA_DEPLOY_TIME` )) -lt $(( 1*90*60 )) ]; then
+        if [ `ls /critical_state/ | grep satcomm | grep -v crit-state | wc -l` -ne 0 ]; then
+            log "disabling satcomm"
+            #rm /critical_state/satcom*
+        fi
+    elif [ `ls /critical_state/ | grep satcom | grep -v crit-state | wc -l` -eq 0 ]; then
+        log "Enabling satcomm"
+        #satcomm-unsquelch
+        #satcomm.1st-inhibit-off
+        #satcomm.2nd-inhibit-off
+    #else
+         #echo "DEBUG: No change necessary to satcomm"
     fi
 
     # Antenna deployment proceedure, occurs after a
     #  full orbit at Cal Poly's recommendation
-    if [ `cat $ANTENNA_FLAG` -ne 1 && getTime -ge 1*90*60 ]; then
+    if [ `cat $ANTENNA_FLAG` -ne 1 ] && [ `getTime` -gt $(( 1*90*60 )) ]; then
         log "Deploying antenna" 
         # sys-deploy-now -n 20
         sleep 20
@@ -92,21 +102,13 @@ while [ `cat $ALL_DEPLOY_COMPLETE_FLAG` -ne 1 ]; do
 
         CURDIR=$PWD
         cd /sdcard
-        uvccapture -M -j -x2048 -y1536 -oantennaDeployedPhoto
+        #uvccapture -M -j -x2048 -y1536 -oantennaDeployedPhoto
         cd $CURDIR
-    fi
-
-    # Enable satcomm communication if the antennas have been deployed
-    if [ `cat $ANTENNA_FLAG` -eq 1 ]; then
-        log "Enabling satcomm"
-        #satcomm-unsquelch
-        #satcomm-1st-inhibit-off
-        #satcomm-2nd-inhibit-off
     fi
 
     # This needs to be completed and the time delay set to the 
     #  proper value
-    if [ `getTime` -ge 1*90*60 ]; then
+    if [ `getTime` -gt $(( 1*120*60 )) ] && [ `cat $DSA_DEPLOYED_FLAG` -ne 1 ]; then
         log "Deploying DSA's"
         #PUTDEPLOYSCRIPTHERE
         # Set the DSA DEPLOY FLAG to 1 once finished
@@ -114,16 +116,16 @@ while [ `cat $ALL_DEPLOY_COMPLETE_FLAG` -ne 1 ]; do
     
     # This checked if all the deployments have succeeded and if
     #  so exits the deployment script
-    CAN_DEPLOY=1
+    CAN_DEPLOY=0
     for FILE in $ANTENNA_FLAG $SATCOMM_FLAG $DSA_DEPLOYED_FLAG; do
-        CAN_DEPLOY=( $CAN_DEPLOY * `cat $(FILE) )
+        CAN_DEPLOY=$(( $CAN_DEPLOY + `cat $FILE` ))
     done
-    if [ $CAN_DEPLOY -eq 1 ]; then
+    if [ $CAN_DEPLOY -eq 3 ]; then
         log "Deployment complete"
         echo "1" > $ALL_DEPLOY_COMPLETE_FLAG
     fi
 done
 
-
+exit 0
 
 
