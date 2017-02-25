b91ed08059bd50f84e7c72d1cb2efc0e57efaba4
Maurice Ling <maurice@linglink.com>
2017-02-25 15:32:32 -0800

    Add i2c commands, i2ctools compile

--- ccardctl/CCardI2CX.h
+++ ccardctl/CCardI2CX.h
@@ -4,7 +4,7 @@
 namespace IrvCS
 {
   /**
-   * Manage I2C expander state
+   * Manage I2C expander state.  @see https://www.kernel.org/doc/Documentation/i2c/dev-interface
    **/
   class CCardI2CX
   {
@@ -13,8 +13,32 @@ namespace IrvCS
      * Initialize the I2C expander state
      **/
     CCardI2CX(uint8_t state);
-
+    
     ~CCardI2CX();
+
+    /**
+     * Set the state
+     * @return 0 if successful,
+     * @return < 0 if error
+     **/
+    int setState(uint8_t state);
+
+    /**
+     * Get the state
+     * @return 0 if successful,
+     * @return < 0 if error
+     **/
+    int getState(uint8_t &state);
+    
+    /**
+     * Determine if our expander is ok.
+     **/
+    bool isOk();
+
+  private:
+    int i2cdev_;
+    int addr_;
+    bool initialized_;
   };
 }
 
