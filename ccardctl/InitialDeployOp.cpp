5ee2bd0536aa2431765fc869ece53714ad73de5d
Joe Banko <joebanko15@gmail.com>
2017-03-20 21:51:20 -0700

    DeployOp Execution Code
    Added code for execution of deploy and release.

--- ccardctl/InitialDeployOp.cpp
+++ ccardctl/InitialDeployOp.cpp
@@ -14,25 +14,73 @@ namespace IrvCS
   {
   }
 
+  OpStatus release(DsaId id){
+    //Release 3 times, 45 seconds timeout each, return if successful
+    for (int i = 0; i < 3; i++) {
+
+      DBG_print(LOG_INFO, "Attempt %d", (i+1));
+      int relStat = controller_ ->
+      performDsaOperation(id, Release, RELEASE_TIMEOUT);
+      if(relStat == StatOk){
+        DBG_print(LOG_INFO, "Success");
+        return StatOk;
+      }
+      DBG_print(LOG_INFO, "Failure");
+      Sleep(5);
+    }
+
+    //Emergency Release of 45+15 seconds
+
+      DBG_print(LOG_INFO, "Attempting Emergency Release");
+    int relStat = controller_ ->
+    performDsaOperation(id, Release, EMERGENCY_RELEASE_TIMEOUT);
+    if(relStat == StatOk){
+      DBG_print(LOG_INFO, "Success");
+      return StatOk;
+    }
+
+    DBG_print(LOG_INFO, "Failure");
+    return relStat;
+  }
+
+  OpStatus deploy(DsaId id){
+    OpStatus status = controller_->performDsaOperation(id, Deploy, DSA_TIMEOUT)
+    if(status == StatOk){
+      DBG_print(LOG_INFO, "Success");
+      return StatOk;
+    }else{
+      DBG_print(LOG_INFO, "Failure");
+      return status;
+    }
+  }
+
   OpStatus InitialDeployOp::execute()
   {
     DBG_print(LOG_INFO, "Executing Initial Deploy Operation");
-    OpStatus status=StatErr;
+    OpStatus status=StatOk;
 
-    //@TODO Implement
-    //
-    // Release DSA1 3 times with timeout 45 sec
-    // Emergency release additional 15 sec if necessary
-    // Release DSA2 3 times with timeout 45 sec
-    // Emergency release 15 sec if necessary
-    // 
-    // Deploy DSA1 timeout 10 sec
-    // Deploy DSA2 timeout 10 sec
-    //
-    // Sleep 5 seconds between each operation
-    //
-    DBG_print(LOG_ERR, "IMPLEMENT ME");
-  
+
+        // Release DSA1 3 times with timeout 45 sec
+        // Emergency release additional 15 sec if necessary
+    DBG_print(LOG_INFO, "Attempting to release DSA 1");
+    OpStatus rel1 = release(DSA_1);
+    Sleep(5);
+        // Release DSA2 3 times with timeout 45 sec
+        // Emergency release 15 sec if necessary
+    DBG_print(LOG_INFO, "Attempting to release DSA 2");
+    OpStatus rel2 = release(DSA_2);
+    Sleep(5);
+
+        // Deploy DSA1 timeout 10 sec
+    DBG_print(LOG_INFO, "Attempting to deploy DSA 1");
+    OpStatus dep1 = deploy(DSA_1);
+    Sleep(5);
+
+        // Deploy DSA2 timeout 10 sec
+    DBG_print(LOG_INFO, "Attempting to deploy DSA 2");
+    OpStatus dep2 = deploy(DSA_2);
+    Sleep(5);
+
     DBG_print(LOG_INFO, "Completed Initial Deploy Operation with status %d", status);
 
     return status;
--- ccardctl/InitialDeployOp.h
+++ ccardctl/InitialDeployOp.h
@@ -4,6 +4,10 @@
 #include "DsaController.h"
 #include "ccardDefs.h"
 
+#define RELEASE_TIMEOUT 45
+#define EMERGENCY_RELEASE_TIMEOUT 60
+#define DSA_TIMEOUT 10
+
 namespace IrvCS
 {
   /**
@@ -15,7 +19,7 @@ namespace IrvCS
     InitialDeployOp(DsaController *controller);
 
     ~InitialDeployOp();
-    
+
     /**
      * Execute the operation
      * @return OpStatus
@@ -23,6 +27,8 @@ namespace IrvCS
     OpStatus execute();
 
   private:
+    OpStatus release(DsaId id);
+    OpStatus deploy(DsaId id);
     DsaController *controller_;
   };
 }
