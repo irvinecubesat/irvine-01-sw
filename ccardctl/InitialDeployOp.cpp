d5c4d028298f0bc2111d480b3389649f5caf392a
Ashwin Gupta <niwhsa9@gmail.com>
2017-03-24 07:00:37 -0700

    fixed test for Initial Deploy

--- ccardctl/InitialDeployOp.cpp
+++ ccardctl/InitialDeployOp.cpp
@@ -1,5 +1,8 @@
 #include "InitialDeployOp.h"
 #include <polysat/debug.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
 
 namespace IrvCS
 {
@@ -26,13 +29,13 @@ namespace IrvCS
         return StatOk;
       }
       DBG_print(LOG_INFO, "Failure");
-      Sleep(5);
+      sleep(5);
     }
 
     //Emergency Release of 45+15 seconds
 
       DBG_print(LOG_INFO, "Attempting Emergency Release");
-    int relStat = controller_ ->
+    OpStatus relStat = controller_ ->
     performDsaOperation(id, Release, EMERGENCY_RELEASE_TIMEOUT);
     if(relStat == StatOk){
       DBG_print(LOG_INFO, "Success");
@@ -44,7 +47,7 @@ namespace IrvCS
   }
 
   OpStatus InitialDeployOp::deploy(DsaId id){
-    OpStatus status = controller_->performDsaOperation(id, Deploy, DSA_TIMEOUT)
+    OpStatus status = controller_->performDsaOperation(id, Deploy, DSA_TIMEOUT);
     if(status == StatOk){
       DBG_print(LOG_INFO, "Success");
       return StatOk;
@@ -64,25 +67,25 @@ namespace IrvCS
         // Emergency release additional 15 sec if necessary
     DBG_print(LOG_INFO, "Attempting to release DSA 1");
     OpStatus rel1 = release(DSA_1);
-    Sleep(5);
+    sleep(5);
         // Release DSA2 3 times with timeout 45 sec
         // Emergency release 15 sec if necessary
     DBG_print(LOG_INFO, "Attempting to release DSA 2");
     OpStatus rel2 = release(DSA_2);
-    Sleep(5);
+    sleep(5);
 
         // Deploy DSA1 timeout 10 sec
     DBG_print(LOG_INFO, "Attempting to deploy DSA 1");
     OpStatus dep1 = deploy(DSA_1);
-    Sleep(5);
+    sleep(5);
 
         // Deploy DSA2 timeout 10 sec
     DBG_print(LOG_INFO, "Attempting to deploy DSA 2");
     OpStatus dep2 = deploy(DSA_2);
-    Sleep(5);
+    sleep(5);
 
     if(dep2!=StatOk || dep1!=StatOk || rel1!=StatOk || rel2!=StatOk){
-      status = StatErr;
+      status = StatTimeOut;
     }
 
     DBG_print(LOG_INFO, "Completed Initial Deploy Operation with status %d", status);
