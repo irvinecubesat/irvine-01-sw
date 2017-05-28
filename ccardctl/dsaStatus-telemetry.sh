d9477740de05813fbab0553bd6fc84a3a95ebd1a
Maurice Ling <maurice@linglink.com>
2017-05-27 18:01:23 -0700

    Allow disabling of snap scenarios via /sdcard/snap.disable.{scenario} use /sys/class/gpio instead of gpiotest, which affects ccardctl dsa status tweak dsaStatus telemetry config which was missing a new line at the end

--- ccardctl/dsaStatus-telemetry.sh
+++ ccardctl/dsaStatus-telemetry.sh
@@ -3,9 +3,15 @@
 # Collect DSA Deploy telemetry
 #
 
+gpioRoot=/sys/class/gpio/
+getGpio()
+{
+    gpioDir=$gpioRoot/gpio${1}
+    cat "${gpioDir}/value"
+}
 outputState()
 {
-    echo "$1=$(gpiotest $2 in)"
+    echo "$1=$(getGpio "$2")"
 }
 
 outputState "D1-R" 58
