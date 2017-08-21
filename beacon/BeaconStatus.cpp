d265321cb89ae1d3cc8f4281227a368cd99e3278
Ashwin Gupta <ashwingupta2000@gmail.com>
2017-08-21 12:54:41 -0700

    removed whitespace in attr=value pairs, replaced with underscore

--- beacon/BeaconStatus.cpp
+++ beacon/BeaconStatus.cpp
@@ -39,19 +39,19 @@ namespace IrvCS
   {
     return os<<"ID = "<<beaconStatus.id_<<std::endl
              <<"LDC = "<<beaconStatus.ldc_<<std::endl
-             <<"Gyro X = "<<beaconStatus.gyro_[0]<<std::endl
-             <<"Gyro Y = "<<beaconStatus.gyro_[1]<<std::endl
-	     <<"Gyro Z = "<<beaconStatus.gyro_[2]<<std::endl 
-             <<"Mag X = "<<beaconStatus.mag_[0]<<std::endl 
-             <<"Mag Y = "<<beaconStatus.mag_[1]<<std::endl 
-             <<"Mag Z = "<<beaconStatus.mag_[2]<<std::endl 
-	     <<"Temp C = "<<beaconStatus.tempDaughterA_<<std::endl
-	     <<"3V Payload Temp C = "<<beaconStatus.tempThreeVpl_<<std::endl
-	     <<"External Temp C = "<<beaconStatus.tempNz_<<std::endl
-	     <<"3V Sensor Voltage = "<<beaconStatus.volt3V_<<std::endl
-	     <<"3V Sensor Current = "<<beaconStatus.curr3V_<<std::endl
-	     <<"5V Payload Voltage = "<<beaconStatus.volt5Vpl_<<std::endl 
-	     <<"5V Payload Current = "<<beaconStatus.curr5Vpl_<< std::endl 
+             <<"Gyro_X="<<beaconStatus.gyro_[0]<<std::endl
+             <<"Gyro_Y="<<beaconStatus.gyro_[1]<<std::endl
+	     <<"Gyro_Z="<<beaconStatus.gyro_[2]<<std::endl 
+             <<"Mag_X="<<beaconStatus.mag_[0]<<std::endl 
+             <<"Mag_Y="<<beaconStatus.mag_[1]<<std::endl 
+             <<"Mag_Z="<<beaconStatus.mag_[2]<<std::endl 
+	     <<"Temp_C="<<beaconStatus.tempDaughterA_<<std::endl
+	     <<"3V_Payload_Temp_C="<<beaconStatus.tempThreeVpl_<<std::endl
+	     <<"External_Temp_C="<<beaconStatus.tempNz_<<std::endl
+	     <<"3V_Sensor_Voltage="<<beaconStatus.volt3V_<<std::endl
+	     <<"3V_Sensor_Current="<<beaconStatus.curr3V_<<std::endl
+	     <<"5V_Payload_Voltage="<<beaconStatus.volt5Vpl_<<std::endl 
+	     <<"5V_Payload_Current="<<beaconStatus.curr5Vpl_<< std::endl 
 
 	     ;
   }
