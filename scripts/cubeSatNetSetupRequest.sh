2a2916d846ebeaaa7afa67d6244f9f5ef08c7424
Maurice Ling <Maurice@linglink.com>
2017-12-15 23:31:40 -0800

    ICS-5 Add safeguard against echo off if interrupted in middle of cubeSatNetRequest.sh.  Also add conversion of old keyInfo file to new irvinecubesat.keyInfo

--- scripts/cubeSatNetSetupRequest.sh
+++ scripts/cubeSatNetSetupRequest.sh
@@ -2,10 +2,35 @@
 instdir=$(cd $(dirname "$0"); pwd)
 topdir=${instdir}/..
 
+OLD_KEYINFO_FILE=~/.irvine-01.keyInfo
 KEYINFO_FILE=~/.irvinecubesat.keyInfo
 CUBESATNET_SETUP_REQUEST=cubesatSetupRequest.txt
 CUBESAT_ADMIN_CERT=auth/cubesat-admin.cert
 KEYTOOL=$topdir/scripts/opensslKeyTool.sh
+
+log()
+{
+    echo $*
+}
+
+#
+# In some cases if you interrupt the script, echo will be turned off.
+# Ensure that echo is turned on.
+#
+reEnableEcho()
+{
+    stty echo
+}
+
+trap reEnableEcho EXIT
+
+if [ -e "$OLD_KEYINFO_FILE" ]; then
+    log "[I] Converting $OLD_KEYINFO_FILE to $KEYINFO_FILE"
+    mv "$OLD_KEYINFO_FILE" "$KEYINFO_FILE"
+    if [ $? -ne 0 ]; then
+	log "[E] Unable to move $OLD_KEYINFO_FILE to  $KEYINFO_FILE"
+    fi
+fi
 
 while [ ! "ok" = "$ok" ]; do
     echo "Enter a secure password which you will use to access CubeSatNet:" 
